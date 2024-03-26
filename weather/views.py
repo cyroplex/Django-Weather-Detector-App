20a4014f4e843f66b5314171b2c456795b1669fa
cyroplex <iemz2003@gmail.com>
2024-03-27 00:52:27 +0300

    django web detector app

---
+++ weather/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+import requests
+import pprint
+
+# Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city'].title()
+        if city != '':
+            response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=e2d993d3db9fab44270d4d53a573aad6')
+            response.raise_for_status()
+            weather_data = response.json()
+            data = {
+                'country_code': str(weather_data['sys']['country']),
+                'coordinate': str(weather_data['coord']['lon']) + ' ' + str(weather_data['coord']['lat']),
+                'temp': str(weather_data['main']['temp']),
+                'pressure': str(weather_data['main']['pressure']),
+                'humidity': str(weather_data['main']['humidity']),
+                }
+            
+            return render(request, 'index.html', {'city':city, 'data':data})
+    else:
+        city = ''
+    return render(request, 'index.html', {'city':city})
